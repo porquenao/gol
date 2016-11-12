827bc7c5fa9541e3e6e34be3981716281f6b8f61
Ademar Alves de Oliveira <ademar111190@gmail.com>
2016-11-12 20:32:01 -0200

    Remove TextUtils dependencie to allow pure JUnit tests The internal code join do the same that TextUtils.join does without Android dependencie

--- src/test/java/mobi/porquenao/gol/test/JUnitTest.java
+++ src/test/java/mobi/porquenao/gol/test/JUnitTest.java
@@ -1,5 +1,7 @@
 package mobi.porquenao.gol.test;
 
+import android.support.annotation.NonNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -11,6 +13,20 @@ public class JUnitTest {
 
     @Test
     public void testJUnit() {
+        Gol gol = new Gol();
+
+        // In JUnit tests we need to remove the default writer once it has a LogCatWriter
+        gol.getWriters().clear();
+
+        // Optional, we can add an STDOUT log to test
+        gol.addWriter(new Gol.Writer() {
+            @Override
+            public void write(int priority, @NonNull String tag, @NonNull String message) {
+                System.out.println(String.format("%s: %s", tag, message));
+            }
+        });
+
+        Gol.setDefault(gol);
         Gol.l("Logging");
     }
 
